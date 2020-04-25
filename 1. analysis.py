00f0ff093d4aa9fd6c0c94ab614ba2e0bf96fb5a
Colm Higgins <G00287906@gmit.ie>
2020-04-25 22:51:13 +0100

    Function updated for creating histograms of each species of Iris flower and scatterplot of each variable

--- 1. analysis.py
+++ 1. analysis.py
@@ -1,8 +1,19 @@
 ## Colm Higgins
 ## program to analyse data for project
+import iris_funcs as ifs
 
-from iris_funcs import IrSums
-    
-IrSums("Iris-setosa")
-IrSums("Iris-versicolor")
-IrSums("Iris-virginica")
+def CreateSum():
+    ifs.IrSums("Iris-setosa")
+    ifs.IrSums("Iris-versicolor")
+    ifs.IrSums("Iris-virginica")
+#CreateSum()
+
+def Createhist():
+    ifs.IrHist("Iris-setosa")
+    ifs.IrHist("Iris-versicolor")
+    ifs.IrHist("Iris-virginica")
+Createhist()
+#with open ('summary.txt', 'w') as f:
+  #  f.write(CreateSum(),file=f)
+
+
--- iris_funcs.py
+++ iris_funcs.py
@@ -1,27 +1,35 @@
 # This will contain the functions to be used in the analysis program
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+heading=["sepal-length","sepal-width","petal-length","petal-width","Species"]
+Data = pd.read_csv('iris.data',names = heading)
+ 
+
+sns.distplot( Data["sepal-length"] , color="blue", label="Sepal Length")
+sns.distplot( Data["sepal-width"] , color="red", label="Sepal Width")
+#sns.distplot( Data["petal-length"] , color="green", label="Petal Length")
+#sns.distplot( Data["petal-width"] , color="orange", label="Petal Width")
+plt.show()
+
+sns.pairplot(Data,hue="Species")
+#plt.title("Plot of species vairables")
+plt.show()
+
+
 
 def IrSums(Spec_name):
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    heading=["sepal-length","sepal-width","petal-length","petal-width","Species"]
-    Data = pd.read_csv('iris.data',names = heading)
     Irflowr = Data[Data.Species == Spec_name]
-    print("Summary of {} flower\n".format(Spec_name),Irflowr.describe(),"\n")
-   
-#print(Data)
+   # print("Summary of {} flower\n".format(Spec_name),Irflowr.describe(),"\n")
+    with open ('summary.csv', 'w') as f:
+        print(Irflowr.describe(),file=f)
+    
+def IrHist(Spec_name1): 
+    import matplotlib.pyplot as plt
+    Irflowr = Data[Data.Species == Spec_name1]
+    Irflowr.hist()
+    plt.title("Histogram of Sepal width&length & Petal width&length for {} Flower".format(Spec_name1))
+    plt.show()
 
-#fig,ax = plt.subplots()
-#MeanValues = Data.groupby(['Species']).mean()
-#StdValues = Data.groupby(['Species']).std()
-#minValues = Data.groupby(['Species']).min()
-#maxValues = Data.groupby(['Species']).max()
-#print(MeanValues)
-#print(StdValues)
-#print(minValues)
-#print(maxValues)
-#ax.plot()
-#ax.legend()
-#fig.show()
-#input()
