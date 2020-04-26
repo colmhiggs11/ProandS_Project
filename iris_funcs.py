7caba834a5d9ea19d294fb17c61a9ebb4e0e9daa
Colm Higgins <G00287906@gmit.ie>
2020-04-26 18:15:35 +0100

    Checking format of csv in Github

--- iris_funcs.py
+++ iris_funcs.py
@@ -5,22 +5,21 @@ import numpy as np
 import seaborn as sns
 
 heading=["sepal-length","sepal-width","petal-length","petal-width","Species"]
-Data = pd.read_csv('IRIS.csv',names = heading)
+Data = pd.read_csv('IRIS.csv',header =0,names = heading)
  
-sns.pairplot(Data,hue="Species")
-#plt.title("Plot of species vairables")
-plt.show()
-
 def IrSums():
-    with open('summarySetosa.csv', 'w') as f:
-        print(Data[Data.Species == "Iris-setosa"].describe(),"\n",file = f)
-    with open('summaryVersicolor.csv', 'w') as e:
-        print(Data[Data.Species == "Iris-versicolor"].describe(),"\n",file = e)
-        print("# Summary of Iris virginica flower data\n",Data[Data.Species == "Iris-virginica"].describe(),"\n",file = f)
-    print(Data.describe())
+    Data[Data.Species == "Iris-setosa"].describe().to_csv("Colm.csv")
+   # with open('summary.txt', 'w') as f:
+      #  print("Summary of Iris Setosa flower data\n",Data[Data.Species == "Iris-setosa"].describe(),"\n",file = f)
+      #  print("Summary of Iris versicolor flower data\n",Data[Data.Species == "Iris-versicolor"].describe(),"\n",file = f)
+       # print("Summary of Iris virginica flower data\n",Data[Data.Species == "Iris-virginica"].describe(),"\n",file = f)    
 
 #def IrHist(Spec_name1): 
     #import matplotlib.pyplot as plt
     #Irflowr = Data[Data.Species == Spec_name1]
     #Irflowr.hist()
     #plt.title("Histogram of Sepal width&length & Petal width&length for {} Flower".format(Spec_name1))
+
+sns.pairplot(Data,hue="Species")
+#plt.title("Plot of species vairables")
+plt.show()
